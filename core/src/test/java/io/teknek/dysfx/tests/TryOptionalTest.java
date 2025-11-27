17ed7e962800007ebb008005db92cd225b5a8ea8
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-11-27 11:14:08 -0500

    Demonstrate tooption weakness

---
+++ core/src/test/java/io/teknek/dysfx/tests/TryOptionalTest.java
@@ -0,0 +1,38 @@
+package io.teknek.dysfx.tests;
+
+
+import io.teknek.dysfx.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TryOptionalTest {
+
+    @Test
+    void emptyButNot(){
+        //null is a legitimate value
+        Try<Object> aTry = Try.of(() -> null);
+        assertTrue(aTry.isSuccess());
+        Optional<Object> results = aTry.toOption();
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    void aFailedOption(){
+        Try<Object> aTry = Try.of(() -> Integer.parseInt("stuff"));
+        assertFalse(aTry.isSuccess());
+        Optional<Object> results = aTry.toOption();
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    void aSuccessToOption(){
+        Try<Object> aTry = Try.of(() -> Integer.parseInt("30"));
+        assertTrue(aTry.isSuccess());
+        Optional<Object> results = aTry.toOption();
+        assertEquals(30, results.get());
+    }
+}
