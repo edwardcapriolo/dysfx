864ad1e0c26c9f9c91440e5b0d8c782a58a08061
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-05 18:01:30 -0500

    Equality for try

--- core/src/test/java/io/teknek/dysfx/tests/TryEqualityTest.java
+++ core/src/test/java/io/teknek/dysfx/tests/TryEqualityTest.java
@@ -11,13 +11,22 @@ public class TryEqualityTest {
     Try<Integer> four = Try.of(() -> 4);
     Try<Integer> alsoFour = Try.of(() -> 4);
     Try<Integer> five = Try.of(() -> 5);
-    
+
+    Try<Integer> failure1 = Try.of(()-> Integer.parseInt("four"));
+    Try<Integer> failure2 = Try.of(()-> Integer.parseInt("four"));
+
     @Test
-    void equals(){
+    void successEquals(){
         assertEquals(four, alsoFour);
         assertNotEquals(four, five);
+        assertEquals(four, four);
     }
 
+    @Test
+    void failureEquals(){
+        assertNotEquals(failure1, failure2);
+        assertEquals(failure1, failure1);
+    }
     @Test
     void hashCodes(){
         assertEquals(four.hashCode(), alsoFour.hashCode());
