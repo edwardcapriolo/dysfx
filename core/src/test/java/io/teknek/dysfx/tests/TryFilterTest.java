0df0ceaf64ce7bde01e2cc0424196c6dfbd2dac4
Edward Guy Capriolo <edlinuxguru@gmail.com>
2025-12-05 19:03:26 -0500

    Try harder

--- core/src/test/java/io/teknek/dysfx/tests/TryFilterTest.java
+++ core/src/test/java/io/teknek/dysfx/tests/TryFilterTest.java
@@ -2,14 +2,22 @@ package io.teknek.dysfx.tests;
 
 import io.teknek.dysfx.Success;
 import io.teknek.dysfx.Try;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TryFilterTest {
 
     @Test
     void filterPass(){
-        assertEquals( new Success<>(4), Try.of(()->4).filter(x -> x == 4));
+        assertEquals( new Success<>(4), Try.of(() -> 4).filter(x -> x == 4));
+    }
+
+    @Test
+    void filterFail(){
+        Try<Integer> y = Try.of(() -> 4).filter(x -> x == 5);
+        assertFalse(y.isSuccess());
     }
 }
